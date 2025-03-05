205875f7d56e82fe0d1e750fe7c13a4ff806b893
ehllll <leeeunhye0510@naver.com>
2025-03-05 23:56:22 +0900

    .

---
+++ src/com/example/calculator/App.java
@@ -0,0 +1,74 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+
+            System.out.print("1번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("2번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+
+            System.out.println("num1 = " + num1);
+            System.out.println("num2 = " + num2);
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char sign = scanner.next().charAt(0);
+
+            int result = calculator.calculate(num1, num2, sign);
+            boolean valid = true;
+
+            switch (sign) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다");
+                        valid = false;
+
+                    } else {
+                        result = num1 / num2;
+                    }
+                    break;
+
+                default:
+                    System.out.println("+.-,*,/ 중에 입력해주세요");
+                    valid = false;
+            }
+
+            if (valid) {
+                System.out.println("결과: " + result);
+
+            }
+
+            System.out.println("더 계산하시겠습니까? (yes: 계속, exit: 종료, clear: 최근 결과 삭제): ");
+            String check = scanner.next();
+            if (check.equals("exit")) {
+                System.out.println("계산기를 종료합니다!");
+                break;
+            } else if (check.equals("yes")) {
+                System.out.println("계산을 계속합니다!");
+            } else if (check.equals("clear")) {
+                calculator.clearResult();
+                System.out.println("최근 결과가 초기화되었습니다 / 현재 결과: " + calculator.getLastResults());
+            }
+            }
+        }
+
+
+    }
+
+
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,66 +1,37 @@
 package com.example.calculator;
 
-import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private int lastResult;
 
-        while (true) {
-
-            System.out.print("1번째 숫자를 입력하세요: ");
-            int num1 = scanner.nextInt();
-
-            System.out.print("2번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
-
-            System.out.println("num1 = " + num1);
-            System.out.println("num2 = " + num2);
-
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char sign1 = scanner.next().charAt(0);
-
-            int result = 0;
-            boolean validOperation = true;
-
-            switch (sign1) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("0으로 나눌 수 없습니다");
-                        validOperation = false;
-
-                    } else {
-                        result = num1 / num2;
-                    }
-                    break;
-
-                default:
-                    System.out.println("+.-,*,/ 중에 입력해주세요");
-                    validOperation = false;
-            }
-
-            if (validOperation) {
-                System.out.println("결과: " + result);
-
-            }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료): ");
-            String check = scanner.next();
-            if(check.equals("exit")) {
-                System.out.println("계산기를 종료합니다");
+    public int calculate(int num1, int num2, char sign) {
+        switch (sign) {
+            case '+':
+                lastResult = num1+num2;
                 break;
-            }
+            case '-':
+                lastResult = num1 - num2;
+                break;
+            case '*':
+                lastResult = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("0으로 나눌 수 없습니다!");
+                }
+                lastResult = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("+,-,*,/ 중에 입력하세요!");
         }
-        scanner.close();
 
+        return lastResult;
+    }
+    public int getLastResults() {
+        return lastResult;
+    }
+
+    public void clearResult() {
+        lastResult = 0;
     }
 }
